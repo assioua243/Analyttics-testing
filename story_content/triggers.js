e44486335be7cf50b43979a9653be4bb2b492c98
Omar Assioua <assioua243@gmail.com>
2024-10-15 12:52:57 +0100

    Test up to scenario

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,11 +2,35 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6gZHnx6Alug":
+      case "5Xn43aShu4J":
         Script1();
         break;
-      case "5z59fvMAtCS":
+      case "5mFXhe7OyoL":
         Script2();
+        break;
+      case "6qAp5hhdGk6":
+        Script3();
+        break;
+      case "68mHglYDZgH":
+        Script4();
+        break;
+      case "66sbc15b4bB":
+        Script5();
+        break;
+      case "6jH3cBD5YDS":
+        Script6();
+        break;
+      case "6OlH2tL6bSN":
+        Script7();
+        break;
+      case "5X38xBXmTWk":
+        Script8();
+        break;
+      case "6cLtZAPWTGp":
+        Script9();
+        break;
+      case "5hPF8jN5haX":
+        Script10();
         break;
   }
 }
--- story_content/user.js
+++ story_content/user.js
@@ -48,4 +48,94 @@ window.Script2 = function()
 
 }
 
+window.Script3 = function()
+{
+  gtag('event', 'button_click', {
+  'event_category': 'Buttons',
+  'event_label': 'Open Slide 4',
+  'value': 1
+});
+
+}
+
+window.Script4 = function()
+{
+  gtag('event', 'button_click', {
+  'event_category': 'Buttons',
+  'event_label': 'Open Slide 5',
+  'value': 1
+});
+
+}
+
+window.Script5 = function()
+{
+  gtag('event', 'button_click', {
+  'event_category': 'Buttons',
+  'event_label': 'Open Slide 7',
+  'value': 1
+});
+
+}
+
+window.Script6 = function()
+{
+  // Get the Storyline player object
+var player = GetPlayer();
+
+// Retrieve the value of the Storyline variable (e.g., QuizScore)
+var jsVMB_Mistakes = player.GetVar("VMB_Mistakes"); // Replace "QuizScore" with your variable name
+
+// Send the variable value to GA as a custom event
+gtag('event', 'VMB_Mistakes', {
+  'event_category': 'VMB_Page',
+  'event_label': 'VMB_Mistakes', // You can customize this label
+  'value': jsVMB_Mistakes // The value to send (from Storyline variable)
+});
+
+console.log('Sent Storyline variable to GA4: ' + storylineVariable);
+
+}
+
+window.Script7 = function()
+{
+  gtag('event', 'button_click', {
+  'event_category': 'Buttons',
+  'event_label': 'Open Slide 3',
+  'value': 1
+});
+
+}
+
+window.Script8 = function()
+{
+  gtag('event', 'button_click', {
+  'event_category': 'Buttons',
+  'event_label': 'Open Slide 6',
+  'value': 1
+});
+
+}
+
+window.Script9 = function()
+{
+  gtag('event', 'button_click', {
+  'event_category': 'Buttons',
+  'event_label': 'Open Slide 8',
+  'value': 1
+});
+
+}
+
+window.Script10 = function()
+{
+  gtag('event', 'button_click', {
+  'event_category': 'Buttons',
+  'event_label': 'Open Slide 9
+  ',
+  'value': 1
+});
+
+}
+
 };
